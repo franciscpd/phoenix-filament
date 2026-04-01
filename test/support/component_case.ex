b8cc174b33633382a4230520036dd524545cc933
Francisross Soares de Oliveira <franciscpd@gmail.com>
2026-04-01 08:04:16 -0300

    test: add ComponentCase helper for component rendering tests

---
+++ test/support/component_case.ex
@@ -0,0 +1,21 @@
+defmodule PhoenixFilament.ComponentCase do
+  @moduledoc """
+  Test case for rendering Phoenix function components.
+  Provides helpers for building form fields in tests.
+  """
+
+  use ExUnit.CaseTemplate
+
+  using do
+    quote do
+      import Phoenix.Component, only: [to_form: 2]
+      import Phoenix.LiveViewTest, only: [rendered_to_string: 1]
+
+      @doc "Creates a Phoenix.HTML.Form from a params map for testing."
+      def make_form(params, opts \\ []) do
+        as = Keyword.get(opts, :as, :test)
+        to_form(params, as: as)
+      end
+    end
+  end
+end
